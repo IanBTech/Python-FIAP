a0a3a835b06da5c7d5eb7d7f36e363f33be0d396
IanBTech <ianbtech257@gmail.com>
2025-03-17 20:51:55 -0300

    Primeira aula com códigos

--- aula_2.py
+++ aula_2.py
@@ -8,28 +8,29 @@ print("Meu nome é Ian Batinga Barbosa")
 
 a = 3
 b = 5
-
-print((2 * a) * (3 * b))
+equacao = ((2 * a) * (3 * b))
+print(equacao)
 
 #3. Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
 
 a1 = 6
 a2 = 9
 a3 = 1
-
-print(a1 + a2 + a3)
+soma = (a1 + a2 + a3)
+print(soma)
 
 #4. Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela
 
 num1 = int(input("Escreva um número para somar: "))
 num2 = int(input("Escreva o segundo numero para somar com o anterior: "))
-print("A soma desses números é", num1 + num2)
+soma1 = num1 + num2
+print(f"A soma desses números é {soma1}")
 
 #5. Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
 
 metros = float(input("Escreva uma medida em métros para tornar em milímetros: "))
-
-print(metros * 1000)
+milimetros = metros * 1000
+print(milimetros)
 
 #6. Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total
 # em segundos.
@@ -40,3 +41,13 @@ minutos = int(input("Escreva a quantidade de minutos: "))
 segundos = int(input("Escreva a quantidade de segundos: "))
 
 print("A soma dos dias, horas, minutos, segundos em segundos é: ", (dias * 86400) + (horas * 3600) + (minutos * 60) + segundos)
+
+#7. Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a
+# porcentagem do aumento. Exiba o valor do aumento e do novo salário.
+
+salario = float(input("Escreva o valor do seu salário: "))
+aumento = float(input("Escreva o valor da porcentagem de aumento (apenas numeros): "))
+
+print("O aumento do seu salário foi de R$", ((aumento * salario)/100), "somado com o atual resulta em R$", ((aumento * salario)/100) + salario)
+
+#
---
+++ aula_inicial.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+check1 = 9
+check2 = 7.5
+check3 = 8
+
+media = (check1+check2+check3)/3
+print(f"Média = {media:.2f}")
