7e3efd262e964348efaaac87200f21270c79a66a
IanBTech <ianbtech257@gmail.com>
2025-03-20 19:23:30 -0300

    inicio do exercício 2

---
+++ aula_3/exercicio2.py
@@ -0,0 +1,6 @@
+#2. Escreva um programa que leia três números e que
+# imprima o maior e o menor.
+
+num1 = int(input("Escreva um número: "))
+num2 = int(input("Escreva outro número: "))
+num3 = int(input("Escreva mais um número: "))
