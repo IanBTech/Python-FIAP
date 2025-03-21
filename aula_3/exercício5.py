2483ca7dc338604e561c8c95d8d8bb52210939e1
IanBTech <ianbtech257@gmail.com>
2025-03-21 00:32:09 -0300

    exercicio 5 completo

---
+++ aula_3/exercício5.py
@@ -0,0 +1,18 @@
+#5. Escreva um programa que leia dois números e que pergunte qual operação você deseja realizar.
+# Você deve poder calcular soma, subtração, multiplicação e divisão.
+# Exiba o resultado da operação solicitada.
+
+num1 = float(input('Escreva o primeiro número: '))
+operacao = str(input('Escreva a operação que deseja realizar ((+), (-), (/), (*)): '))
+num2 = float(input('Escreva o segundo número: '))
+
+if operacao == '+':
+    print(f'{num1} + {num2} = {num1 + num2}')
+elif operacao == '-':
+    print(f'{num1} - {num2} = {num1 - num2}')
+elif operacao == '*':
+    print(f'{num1} * {num2} = {num1 * num2}')
+elif operacao == '/':
+    print(f'{num1} / {num2} = {num1 / num2}')
+else:
+    print(f'operação invalida, digite apenas (+), (-), (/), (*).')
