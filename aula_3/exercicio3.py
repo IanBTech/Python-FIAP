47a51c67214cd00fdffaa413629b931764a8ed7e
IanBTech <ianbtech257@gmail.com>
2025-03-20 22:28:42 -0300

    exercício 3 completo

---
+++ aula_3/exercicio3.py
@@ -0,0 +1,11 @@
+#3. Escreva um programa que pergunte o salário do funcionário e calcule
+# o valor do aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%.
+# Para inferiores ou iguais, de 15%.
+
+salario = float(input('Digite seu salário: '))
+calculo1 = ((salario * 10)/100) + salario
+calculo2 = ((salario * 15)/100) + salario
+if salario > 1250:
+    print(calculo1)
+else:
+    print(calculo2)
