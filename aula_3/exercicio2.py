e9642b44c5bc3f98c07c8fd151320a2ab10e9908
IanBTech <ianbtech257@gmail.com>
2025-03-20 22:18:16 -0300

    exercício 2 completo

--- aula_3/exercicio2.py
+++ aula_3/exercicio2.py
@@ -1,6 +1,23 @@
 #2. Escreva um programa que leia três números e que
 # imprima o maior e o menor.
 
-num1 = int(input("Escreva um número: "))
-num2 = int(input("Escreva outro número: "))
-num3 = int(input("Escreva mais um número: "))
+num1 = int(input('Escreva um numero: '))
+num2 = int(input('Escreva um numero: '))
+num3 = int(input('Escreva um numero: '))
+if num1 > num2 > num3:
+    print(num1)
+else:
+    if num1 > num3 > num2:
+        print(num1)
+    else:
+        if num2 > num1 > num3:
+            print(num2)
+        else:
+            if num2 > num3 > num1:
+                print(num2)
+            else:
+                if num3 > num2 > num1:
+                    print(num3)
+                else:
+                    if num3 > num1 > num2:
+                        print(num3)
