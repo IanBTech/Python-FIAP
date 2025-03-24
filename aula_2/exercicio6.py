0be75c918a9717df6e7b25fe670cb973cc6fa5ee
IanBTech <ianbtech257@gmail.com>
2025-03-24 20:22:32 -0300

    aula 2 dividida direito

--- aula_2/exercicio6.py
+++ aula_2/exercicio6.py
@@ -1,9 +1,16 @@
-#6. Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total
-# em segundos.
+#6. Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário.
+# Calcule o total em segundos.
 
-dias = int(input("Escreva a quantidade de dias: "))
-horas = int(input("Escreva a quantidade de horas: "))
-minutos = int(input("Escreva a quantidade de minutos: "))
-segundos = int(input("Escreva a quantidade de segundos: "))
-ssegs = (dias * 86400) + (horas * 3600) + (minutos * 60) + segundos
-print(f"A soma dos dias, horas, minutos, segundos em segundos é: {ssegs}")
+dias = int(input("Valor em dias:"))
+horas = int(input("Valor em horas:"))
+minutos = int(input("Valor em minutos:"))
+segundos = int(input("Valor em segundos:"))
+
+s1 = (dias * 86400)
+s2 = (horas * 3600)
+s3 = (minutos * 60)
+s4 = segundos
+
+soma = s1 + s2 + s3 + s4
+
+print("o valor em segundos é:", soma)
