f584d20bac432a72cff5618aa5cba6f11212322b
IanBTech <ianbtech257@gmail.com>
2025-03-31 19:39:33 -0300

    errei a medida

--- aula_2/exercicio9.py
+++ aula_2/exercicio9.py
@@ -2,7 +2,7 @@
 # Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
 
 d = float(input("Digite a distância a percorrer em km: "))
-vm = float(input("Digite a velociade média do veiculo em h: "))
+vm = float(input("Digite a velociade média do veiculo em km/h: "))
 tempo = d/vm
 
 print(f"O tempo estimado para percorrer o percurso com essa velociadade é de {tempo:.2f} horas")
