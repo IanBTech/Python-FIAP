73977f369408299509546120d90d38033f66ddb7
IanBTech <ianbtech257@gmail.com>
2025-03-31 19:49:48 -0300

    faltou um sinal

--- aula_3/exercicio8.py
+++ aula_3/exercicio8.py
@@ -5,7 +5,7 @@ nota1 = float(input('Escreva a primeira nota: '))
 nota2 = float(input('Escreva a segunda nota: '))
 media = (nota1 + nota2) / 2
 
-if media > 6:
+if media >= 6:
     print('Aprovado')
 else:
     print('Reprovado')
