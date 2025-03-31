ff6ab6e1419adbcef5c1d01fcd53e654c7467789
IanBTech <ianbtech257@gmail.com>
2025-03-31 20:20:20 -0300

    minimizado

--- aula_3/exercicio2.py
+++ aula_3/exercicio2.py
@@ -8,32 +8,20 @@ num3 = int(input('Escreva um numero: '))
 #maior número
 if num1 > num2 > num3:
     print(f'o maior número é {num1}')
-elif num1 > num3 > num2:
-    print(f'o maior número é {num1}')
-elif num2 > num1 > num3:
+elif num1 < num2 > num3:
     print(f'o maior número é {num2}')
-elif num2 > num3 > num1:
-    print(f'o maior número é {num2}')
-elif num3 > num2 > num1:
-    print(f'o maior número é {num3}')
-elif num3 > num1 > num2:
+elif num1 < num2 < num3:
     print(f'o maior número é {num3}')
 else:
     print(f'Algo de errado não está certo')
 
 #menor número
 
-if num1 > num2 > num3:
-    print(f'o menor número é {num3}')
-elif num1 > num3 > num2:
-    print(f'o menor número é {num2}')
-elif num2 > num1 > num3:
-    print(f'o menor número é {num3}')
-elif num2 > num3 > num1:
+if num1 < num2 < num3:
     print(f'o menor número é {num1}')
-elif num3 > num2 > num1:
-    print(f'o menor número é {num1}')
-elif num3 > num1 > num2:
+elif num1 > num2 < num3:
     print(f'o menor número é {num2}')
+elif num1 > num2 > num3:
+    print(f'o menor número é {num3}')
 else:
     print(f'Algo de errado não está certo')
