8ba58490fbbc812f214964b39eb45368e50811eb
IanBTech <ianbtech257@gmail.com>
2025-08-04 19:56:46 -0300

    ex 3

---
+++ aula_5/ex-3.py
@@ -0,0 +1,8 @@
+#3. Modifique o exemplo anterior de forma a realizar a
+# mesma tarefa, mas sem utilizar a variável achou. Dica:
+# observe a condição de saída do while.
+
+L1 = [1]
+L2 = [3]
+L3 = L1 + L2
+print(L3)
