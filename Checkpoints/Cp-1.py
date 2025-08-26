3e47c364833c05e49ede7f7bbfb959a431f1ac2d
IanBTech <ianbtech257@gmail.com>
2025-08-25 21:15:52 -0300

    cp q n deu certo

---
+++ Checkpoints/Cp-1.py
@@ -0,0 +1,11 @@
+#Dupla: Ian Batinga Barbosa / Giovanna Carmona Roman Lopes
+
+# O programa solicite as notas de cada semestre utilizando estruturas de repetição for.
+# As notas sejam armazenadas em listas.
+# Os valores informados sejam validados, aceitando apenas números entre 0 e 10.
+# A menor nota de checkpoint seja descartada utilizando um laço for.
+# A média final seja exibida com 1 casa decimal.
+
+
+
+
